f6c591a48287b661f09f12a03aff9bc587dd776b
AhsanVirani <ahsan.viran08@gmail.com>
2020-12-13 19:13:12 -0500

    Student class

---
+++ Student.java
@@ -0,0 +1,53 @@
+/**
+access_modifier = {
+	public,
+	private,
+	protected
+}
+
+<access_modifier> class <Class_Name> {
+
+	// instance
+	variable1;
+	variable2;
+	...
+
+	// constructor
+	<access_modifier> <Class_Name>(param1, param2) {
+		variable1 = param1;
+		variable2 = param2;
+	}
+
+	// method1
+
+
+}
+ 
+**/
+
+
+public class Student {
+
+	private String	name;
+	private int	age;
+
+	public Student(String name, int age) {
+		this.name	= name;
+		this.age	= age;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+/**
+	public static void main(String[] args) {
+		Student ahsan = new Student("Ahsan", 22);
+		System.out.println(ahsan.getName());
+	}
+**/
+}
+
+
+
+
---
+++ TestStudent.java
@@ -0,0 +1,8 @@
+public class TestStudent {
+
+	public static void main(String[] args) {
+		Student ahsan = new Student("Ahsan", 22);
+		System.out.println(ahsan.getName());
+	}
+
+}
